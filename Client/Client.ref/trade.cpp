d36e5f6e893c210974daa676d35e0bc3aa9a5f1c
MuffyPuff <sjjeuseen@gmail.com>
2017-08-29 21:09:41 +0200

    Remove .ref
    Removed local temporary files that were committed by accident.

--- Client/Client.ref/trade.cpp
+++
@@ -1,9 +0,0 @@
-#include "trade.h"
-
-Trade::Trade(qint16 tradeID, QString sender, Resources amountOffered, Resources amountRequested, QObject *parent) : QObject(parent)
-{
-    m_tradeID = tradeID;
-    m_sender  = sender;
-    m_amountOffered     = amountOffered;
-    m_amountRequested   = amountRequested;
-}
